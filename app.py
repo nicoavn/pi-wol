c90d734933c894c0a6120700e2bc726c4bc1bce7
Alvin Duran <alvin@fullstacklabs.co>
2025-05-29 21:33:18 -0400

    requirements and endpoint

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+import subprocess
 
+from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/wake', methods=['GET'])
+def wake():
+    mac_address = request.args.get('mac')
+    if not mac_address:
+        return "MAC address required", 400
 
+    try:
+        subprocess.run(["wakeonlan", mac_address], check=True)
+        return f"Magic packet sent to {mac_address}", 200
+    except subprocess.CalledProcessError:
+        return "Failed to send WoL packet", 500
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
